6bb4d1e01f66f1517f5744791971bcf56e8747f1
Mykhailo Bruslik <Mykhailo.Bruslik.temporary@gmail.com>
2022-10-15 13:58:14 +0300

    Minor fixes to PositionGenerator class

--- LOT.BLL/Services/Members/PositionGenerator.cs
+++ LOT.BLL/Services/Members/PositionGenerator.cs
@@ -2,43 +2,42 @@
 using LOT.BLL.Exceptions;
 using LOT.BLL.Models.Members;
 using LOT.BLL.Models.Positions;
-using System.Linq;
 
 namespace LOT.BLL.Services.Members
 {
-    public static class PositionGenerator
+    public class PositionGenerator
     {
-        private static Random random = new();
-        private static PositionService service = new();
-        public static PositionModel GeneratePosition(MemberModel member)
-        {
-            if (member.Positions.Count >= 2)
-                throw new PositionServicesException("Member already know maximum positions he can!");
-            else
-            {
-                foreach (PositionModel existingPosition in member.Positions)
-                {
-                    if (existingPosition.Name == newPositionName)
-                        throw new PositionServicesException("Error! This member is already can play this position!");
-                }
+        private Random random = new();
+        private PositionService service = new();
+        //public static PositionModel GeneratePosition(MemberModel member)
+        //{
+        //    if (member.Positions.Count >= 2)
+        //        throw new PositionServicesException("Member already know maximum positions he can!");
+        //    else
+        //    {
+        //        foreach (PositionModel existingPosition in member.Positions)
+        //        {
+        //            if (existingPosition.Name == newPositionName)
+        //                throw new PositionServicesException("Error! This member is already can play this position!");
+        //        }
 
-                member.Positions.Add(newPosition);
-            }
-        }
+        //        member.Positions.Add(newPosition);
+        //    }
+        //}
 
-        private static PositionModel CreatePosition(int memberLevel = 1)
+        private PositionModel CreatePosition(int memberLevel = 1)
         {
             PositionModel position = CreateEmptyPosition(memberLevel);
             position.Name = GetNewRandomPositionName();
-            service.Add(position);
-            return service.Get(position);
+            // No need to adding postition in DB, before assigning position to player
+            return position;
         }
 
         /// <summary>
         /// Method create new empty position object without Name and Id.
         /// </summary>
         /// <returns>Empty PositionModel object.</returns>
-        private static PositionModel CreateEmptyPosition(int memberLevel = 1)
+        private PositionModel CreateEmptyPosition(int memberLevel = 1)
         {
             var position = new PositionModel()
             {
@@ -57,7 +56,7 @@ namespace LOT.BLL.Services.Members
         /// Method generate random name for new position for member.
         /// </summary>
         /// <returns>Random PositionName enum.</returns>
-        private static PositionsNames GetNewRandomPositionName()
+        private PositionsNames GetNewRandomPositionName()
         {
             Array names = Enum.GetValues(typeof(PositionsNames));
             PositionsNames newName = (PositionsNames)names.GetValue(random.Next(names.Length));
@@ -70,14 +69,13 @@ namespace LOT.BLL.Services.Members
         /// </summary>
         /// <param name="exceptedPosition">A position that the member already knows.</param>
         /// <returns>Random PositionName enum.</returns>
-        private static PositionsNames GetNewRandomPositionName(PositionsNames exceptedPosition)
+        public PositionsNames GetNewRandomPositionName(PositionsNames exceptedPosition)
         {
-            Array names = Enum.GetValues(typeof(PositionsNames));
+            List<PositionsNames> positionNames = Enum.GetValues(typeof(PositionsNames)).Cast<PositionsNames>().ToList();
             
-            names = names.Where(x => x != exceptedPosition)
-                         .ToArray();
-            PositionsNames newName = (PositionsNames)names
-                        .GetValue(random.Next(names.Length));
+            var filteredPositionNames = positionNames.Where(x => x != exceptedPosition);
+
+            PositionsNames newName = positionNames[random.Next(positionNames.Count)];
             return newName;
         }
     }
---
+++ LOT.Tests/BusinessLogicTests/PositionGeneratorTests.cs
@@ -0,0 +1,20 @@
+﻿using LOT.BLL.Services.Members;
+
+namespace LOT.Tests.BusinessLogicTests
+{
+    [TestClass]
+    internal class PositionGeneratorTests
+    {
+        [TestMethod]
+        public void GetNewRandomPositionNameGeneratesPosition()
+        {
+            var positionGenerator = new PositionGenerator();
+            var position = BLL.Enums.PositionsNames.Mid;
+
+            var generatedPosition = positionGenerator.GetNewRandomPositionName(position);
+
+            Assert.IsNotNull(generatedPosition);
+            Assert.AreNotEqual(position, generatedPosition);
+        }
+    }
+}
